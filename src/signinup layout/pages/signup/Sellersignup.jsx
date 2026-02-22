e4a4cdd357ccfe19935bc040eec36e59c6c51813
emananwar2003 <eman.anwar.altohamy.2003@gmail.com>
2026-02-22 22:06:09 +0200

    new signup files

---
+++ src/signinup layout/pages/signup/Sellersignup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sellersignup = () => {
+  return (
+    <div>
+      seller sign up
+    </div>
+  )
+}
+
+export default Sellersignup
--- src/signinup layout/signlayout.jsx
+++ src/signinup layout/signlayout.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
-import Signup from './pages/signup/signup';
 import Sellerlogin from "./pages/login/Sellerlogin.jsx"
 import Userlogin from './pages/login/userlogin.jsx';
+import Usersignup from './pages/signup/Usersignup.jsx';
+import Sellersignup from './pages/signup/Sellersignup.jsx';
+
 const Signlayout = () => {
     return (
-        <div>
-            <Routes>
-                <Route path="userlogin" element={<Userlogin/>} />
-                <Route path="sellerlogin" element={<Sellerlogin />} />
-                <Route path="signup" element={<Signup />} />
-            </Routes>
-        </div>
+      <div>
+        <Routes>
+          <Route path="userlogin" element={<Userlogin />} />
+          <Route path="sellerlogin" element={<Sellerlogin />} />
+          <Route path="sellersignup" element={<Sellersignup />} />
+          <Route path="usersignup" element={<Usersignup/>} />
+        </Routes>
+      </div>
     );
 }
 
