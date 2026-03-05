86d44bdb8f57b9e319e570858ced1583a398d39b
emananwar2003 <eman.anwar.altohamy.2003@gmail.com>
2026-03-05 16:27:31 +0200

    profile avatar

--- src/adminlayout/Adminlayout.jsx
+++ src/adminlayout/Adminlayout.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Admindashboard from './pages/Admindashboard';
-
+import Navbars from '../userlayout/usercomponents/Navbar';
 
 const Adminlayout = () => {
   return (
     <div>
+      <Navbars/>
       <Routes>
         <Route index element={<Admindashboard/>} />
       </Routes>
